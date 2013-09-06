bc9ea6e4ec61988941623da84713da54b8f1cef1
Peter Pan <deeplovepan@gmail.com>
2013-09-06 14:24:15 +0800

    first commit

---
+++ TableReorderAndDeleteTest/TestTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestTableViewController.h
+//  TableReorderTest
+//
+//  Created by Pan Peter on 13/9/6.
+//  Copyright (c) 2013年 Pan Peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestTableViewController : UITableViewController
+
+@end
---
+++ TableReorderAndDeleteTest/TestTableViewController.m
@@ -0,0 +1,127 @@
+//
+//  TestTableViewController.m
+//  TableReorderTest
+//
+//  Created by Pan Peter on 13/9/6.
+//  Copyright (c) 2013年 Pan Peter. All rights reserved.
+//
+
+#import "TestTableViewController.h"
+
+static NSString *CellIdentifier = @"Cell";
+
+
+@interface TestTableViewController ()
+{
+    NSMutableArray *dataArray;
+}
+@end
+
+@implementation TestTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CellIdentifier];
+    dataArray = [[NSMutableArray alloc] init];
+    int i;
+    for(i=0; i<20; i++)
+    {
+        [dataArray addObject:@(i)];
+    }
+    self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return dataArray.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    NSNumber *number = dataArray[indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", number];
+    
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [dataArray removeObjectAtIndex:indexPath.row];
+    [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+}
+
+
+
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+    NSNumber *data = dataArray[fromIndexPath.row];
+    [dataArray removeObjectAtIndex:fromIndexPath.row];
+    [dataArray insertObject:data atIndex:toIndexPath.row];
+}
+
+
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+@end
